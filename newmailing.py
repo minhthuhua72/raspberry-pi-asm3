44c3d8f0c7d99499815bf47289b8f30ea2ab4518
minhthuhua72 <73808947+minhthuhua72@users.noreply.github.com>
2022-01-14 23:40:14 +0700

    Update newmailing.py

--- newmailing.py
+++ newmailing.py
@@ -1,27 +1,28 @@
 import smtplib
 from smtplib import SMTP
 from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from email.mime.multipart import MIMEMultipart 
 
 recipients = ["jesshua722@gmail.com"]
 
 def send_email():
     from_adr = "jesshua722@gmail.com"
-    server = SMTP('smtp.gmail.com', 587)
+    server = SMTP('smtp.gmail.com', 587) # Defining SMTP server and port
     subject= "Control your light"
     
+    # Format text and content in email 
     msg = MIMEMultipart()
     msg["From"] = from_adr
     msg["To"] = recipients[0]
     msg["Subject"] = subject
-    message = "If you want to manually control the light, go to this website: http://192.168.50.199/index.php"
+    message = "If you want to manually control the light, go to this website: http://192.168.50.199/index.php" 
     msg.attach(MIMEText(message))
 
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
+    server.ehlo() # # Can be omitted
+    server.starttls() # Create an unsecured SMTP connection and encrypt it
+    server.ehlo() # Can be omitted
     server.login("jesshua722@gmail.com", "KIKI&&KICHI4ever")
-    server.sendmail(from_adr, recipients, msg.as_string())
+    server.sendmail(from_adr, recipients, msg.as_string()) # Send email here
     server.quit()
 
 
